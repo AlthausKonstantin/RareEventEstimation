3cbe996a9bdfedca0ce41bc47981ec41504e617b
Konstantin Althaus <konstantin.althaus@tum.de>
2022-07-06 09:43:50 +0200

    different counters for scripts for second VM.

--- cbree_diffusion.sh
+++ cbree_diffusion.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python docs/benchmarking/data/cbree-diffusion-problem.py --dir /app/cbree_sim/diffusion_sim
+python docs/benchmarking/data/cbree-diffusion-problem.py --dir /app/cbree_sim/diffusion_sim --counter 6
