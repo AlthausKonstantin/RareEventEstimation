118a807618143c789ab6778b4c7087daa325d261
Konstantin Althaus <konstantin.althaus@tum.de>
2022-07-09 19:25:42 +0200

    corrected scripts

--- cbree_diffusion_4_6.sh
+++ cbree_diffusion_4_6.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python docs/benchmarking/data/cbree-vmfnm-diffusion-problem.py--dir /app/cbree_sim/diffusion_sim --start 4 --stop 6
+python docs/benchmarking/data/cbree-vmfnm-diffusion-problem.py --dir /app/cbree_sim/diffusion_sim --start 4 --stop 6
