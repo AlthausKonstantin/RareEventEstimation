c2dd77225d041b01d7f238deb6ab4a3ed79c3191
Konstantin Althaus <konstantin.althaus@tum.de>
2022-07-13 18:00:30 +0200

    added counter to scritps.

--- cbree_linear_small_36.sh
+++ cbree_linear_small_36.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python docs/benchmarking/data/cbree-lin-vmfnm.py --dir /app/cbree_sim/toy_problems 
+python docs/benchmarking/data/cbree-lin-vmfnm.py --dir /app/cbree_sim/toy_problems --counter 36
