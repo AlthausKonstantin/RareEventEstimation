8c0d79ad30c113a3ec24265bac5fe47b1c6fea91
Konstantin Althaus <althaus.konstantin.95@gmail.com>
2023-03-11 14:39:43 +0100

    docs: linted conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,29 +6,27 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'rareeventestimation'
-copyright = '2023, Konstantin Althaus'
-author = 'Konstantin Althaus'
-release = '16/05/22'
+project = "rareeventestimation"
+copyright = "2023, Konstantin Althaus"
+author = "Konstantin Althaus"
+release = "16/05/22"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.napoleon',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.napoleon",
     "myst_parser",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 autoclass_content = "both"
 
 # myst settings
-myst_enable_extensions = [
-    "dollarmath"
-]
+myst_enable_extensions = ["dollarmath"]
 
 # Napoleon settings
 napoleon_google_docstring = True
@@ -37,8 +35,8 @@ napoleon_google_docstring = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
 html_theme_options = {
     "repository_url": "https://github.com/AlthausKonstantin/rareeventestimation",
     "use_repository_button": True,
